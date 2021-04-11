0b9d89328f3aeac0eb1367b26d906c24889ce5c8
mbehi <mohsin.behi@gmail.com>
2021-04-11 01:42:00 -0500

    started the first few steps of setting up a server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// most of your actual server definition goes here
+// A server's job is to listen at some path for a particular method
+// listening for GET requests at the path
+app.get('/', (request, response) => {
+  // when we get that request, send a response that says 'hello!'
+  // response has some methods that are very helpful, such as a send method
+  response.send('hello!');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
