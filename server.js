3997ac56d461406fc5ff9f9bbfa18a9ad471632b
mbehi <mohsin.behi@gmail.com>
2021-05-07 22:03:02 -0500

    completed backend

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
+'use strict';
+
 const express = require('express');
-// actually use the .env file I created
 require('dotenv').config();
+const cors = require('cors');
+
 // import the json file
-const weatherData = require('./server.json');
+const weatherData = require('./data/weather.json');
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
 // most of your actual server definition goes here
 // A server's job is to listen at some path for a particular method
 // listening for GET requests at the path
-app.get('/', (request, response) => {
-  // when we get that request, send a response that says 'hello!'
-  // response has some methods that are very helpful, such as a send method
-  response.send('hello!');
+app.get('/weather', (request, response) => {
+  const allDailyForecasts = weatherData.data.map(day => new DailyForecast(day));
+  response.send(allDailyForecasts);
 });
 
-app.get('/server', (request, response) => {
-  response.json(weatherData);
-});
+function DailyForecast(day) {
+  this.date = day.datetime;
+  this.description = day.weather.description;
+}
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
